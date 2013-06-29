3af71fa3909b63bb2a5151f0f4088e11b3dc51ac
Juancete <jjc_arg@hotmail.com>
2013-06-29 00:35:43 -0700

    Se implementa un DSL más verboso.

--- src/Dsl.groovy
+++ src/Dsl.groovy
@@ -1,10 +1,14 @@
 import java.beans.java_awt_BorderLayout_PersistenceDelegate;
 import java.lang.reflect.Type
 
+import org.apache.ivy.core.module.descriptor.ExtendsDescriptor;
+
 import utn.tadp.fontana.Bean;
 import utn.tadp.fontana.Compleja;
 import utn.tadp.fontana.Primitiva;
 import utn.tadp.fontana.configuracion.Configuracion;
+import utn.tadp.fontana.estrategia.InicializacionPorConstructor;
+import utn.tadp.fontana.estrategia.InicializacionPorSetters;
 import utn.tadp.fontana.Persona;
 
 class Dsl {
@@ -14,19 +18,61 @@ class Dsl {
 	def prop
 	def tipo
 
-	//	def propertyMissing(String name){
-	//		if (name=="fin"){
-	//			config.addBean(nombre, obj)
-	//			println "paso"
-	//		}
-	//		else {
-	//			throw MissingPropertyException(name)
-	//		}
-	//	}
+//		def propertyMissing(String name){
+//			if (name=="valor"){
+//				invokeMethod(name, valor)
+//			}
+//			else{
+//				throw new MissingPropertyException("No se encuentra la propiedad " + name)
+//			}
+//		}
 
 
 	static Configuracion config = new Configuracion();
+	def static Objeto = "Objeto"
+	def static la = "la"
+	def static conoce = "conoce"
+	def static como = "como"
+	def static ademas = "esto es fruta"
+	def static constructor = "constructor"
+	def static accessors = "accessors"
+	
+	public llamado (String nombreDelObjeto){
+		this.nombre= nombreDelObjeto
+		this
+	}
 
+	public mediante (String propertyName){
+		this.propertyName(propertyName)
+	}
+	
+	public propiedad (String propertyName){
+		this.prop= propertyName
+		this
+	}
+	
+	public con (String n){
+		this
+	}
+
+	public teniendo (String n){
+		this
+	}
+	
+	public de (String n){
+		this
+	}
+	public que (String n){
+		this
+	}
+	public un (String n){
+		this	
+	}
+	
+	public y (String n){
+		this
+	}
+	
 	public beanId(String n) {
 		this.nombre= n
 		return this
@@ -51,17 +97,41 @@ class Dsl {
 		this.tipo= c
 		return this
 	}
-	public valor(n){
+	
+	public valor(String unString){
+		obj.addDependencia(prop, new Primitiva(String, unString))
+	}
+	
+	public valor(int unEntero){
+		obj.addDependencia(prop, new Primitiva(int, unEntero))
+	}
 
-		if (tipo !=String && tipo !=int && tipo !=boolean ){
-
-			obj.addDependencia(prop, new Bean( n,config))
-		}else{
-
-			obj.addDependencia(prop, new Primitiva(tipo, n))
+	public valor(boolean unBoolean){
+		obj.addDependencia(prop, new Primitiva(boolean, unBoolean))
+	}
+	
+//	public valor(n){
+//
+//			obj.addDependencia(prop, new Bean( n,config))
+//
+//		return this
+//	}
+	public a (String unBeanName){
+		obj.addDependencia(prop, new Bean( unBeanName,config))
+		this
+	}
+	
+	public por(String unString){
+		if(unString == "constructor"){
+			InicializacionPorConstructor inicializacionDefault = new InicializacionPorConstructor();
+			inicializacionDefault.setMiDependenciaCompleja(obj);
 		}
-
-		return this
+		if(unString == "accessors"){
+			InicializacionPorSetters inicializacionDefault = new InicializacionPorSetters();
+			inicializacionDefault.setMiDependenciaCompleja(obj);
+		}else
+		{
+		throw new RuntimeException()
+		}
 	}
 }
-
--- test/Prueba.groovy
+++ test/Prueba.groovy
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static Dsl.*;
 class Prueba {
 
-	@Test
+	//@Test
 	public void test1() {
 		
 		def dsl= new Dsl()
@@ -22,17 +22,46 @@ class Prueba {
 		Assert.assertEquals(true, fiestera.getVive())
 	}
 
+//	con las propiedades ["nombre","edad"] de valores ["pepe",100]
+	@Test
+	public void test3() {
+
+		def dsl= new Dsl()
+		dsl.definir {
+			
+			un Objeto llamado "pepe" de la clase Persona por accessors
+			
+			con la propiedad "nombre" teniendo como valor "pepe"
+			y ademas con la propiedad "edad" teniendo como valor 100
+			y ademas con la propiedad "vive" teniendo como valor true
+			que conoce mediante "conocido" a "el tio"
+			
+			
+			un Objeto llamado "el tio" de la clase Persona
+			y ademas con la propiedad "nombre" teniendo como valor "gorge"
+			y ademas con la propiedad "vive" teniendo como valor true
+		}
+		Persona fiestera = (Persona)config.getBean("pepe")
+		Persona fiestero = (Persona) config.getBean("el tio")
+		Assert.assertEquals("pepe", fiestera.getNombre())
+		Assert.assertEquals(100, fiestera.getEdad())
+		Assert.assertEquals(true, fiestero.getVive())
+		Assert.assertEquals("gorge", fiestero.getNombre())
+		Assert.assertEquals("gorge", fiestera.getConocido().getNombre())
+	}
+	
 	@Test
 	public void test2() {
 
 		def dsl= new Dsl()
 		dsl.definir {
+			
 			beanId "pepo"         clase Persona
 
 			propertyName "nombre"    tipo String    valor "pepe"
 			propertyName "edad"      tipo int       valor 100
 			propertyName "vive"      tipo boolean   valor true
-			propertyName "conocido"  tipo Persona   valor "el tio"
+			propertyName "conocido"  tipo Persona   a "el tio"
 
 
 
